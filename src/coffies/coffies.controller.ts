db88d56af4b93f61c277d8977b7594abb70ff435
tw1ttt3r <tw1ttt3r@gmail.com>
2021-11-26 14:21:21 -0600

    feat: added param route

--- src/coffies/coffies.controller.ts
+++ src/coffies/coffies.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffies')
 export class CoffiesController {
+  // access directly route /coffies
   @Get()
   findAllCoffies() {
     return 'This action returns all the coffies';
   }
 
+  // access directly route /coffies/flavors
   @Get('flavors')
   findAllFlavors() {
     return 'This action returns all the coffies flavors';
+  }
+
+  // access directly route /coffies/flavors/<id>
+  // if you need get a bunch of params use @Param() params: any
+  // if you need get one param use @Param('<name-param>') <name-param>: <type>
+  @Get('flavors/:flavor')
+  findOneFlavors(@Param('flavor') flavor: string) {
+    return `This action returns coffies flavors: ${flavor}`;
   }
 }
